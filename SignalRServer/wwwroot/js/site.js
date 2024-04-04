36c37af4e9ac35ef684d4141bb6badc141e960c2
black-spirit3241 <blackspirit781863@gmail.com>
2024-04-05 00:40:07 +0700

    Add JavaScript Client

--- SignalRServer/wwwroot/js/site.js
+++ SignalRServer/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/learningHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+ connection.start();
+
+connection.on("ReceiveMessage", (message) => {
+    $('#signalr-message-panel').prepend($('<div />').text(message));
+});
+
+$('#btn-broadcast').click(function () {
+    var message = $('#broadcast').val();
+    connection.invoke("BroadCastMessage", message).catch(err =>
+        console.error(err.toString())
+    );
+});
+
+async function start() {
+    try {
+
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
