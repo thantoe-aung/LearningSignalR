6549401f1cf13c636be374887e8959a382423cd5
black-spirit3241 <blackspirit781863@gmail.com>
2024-04-12 22:14:34 +0700

    Add More SignalR Methods to Hub

--- SignalRServer/Hubs/LearningHub.cs
+++ SignalRServer/Hubs/LearningHub.cs
@@ -9,6 +9,42 @@ namespace SignalRServer.Hubs
             await Clients.All.ReceiveMessage(message);
         }
 
+        public async Task SendToOthers(string message)
+        {
+            await Clients.Others.ReceiveMessage(message);  
+        }
+
+        public async Task SendToIndividual(string connectionId,string message)
+        {
+            await Clients.Client(connectionId).ReceiveMessage(GetMessageToSend(message));
+        }
+
+
+        public async Task SendToCaller(string message)
+        {
+            await Clients.Caller.ReceiveMessage(GetMessageToSend(message));
+        }
+
+        public async Task SendToGroup(string groupName,string maessage)
+        {
+            await Clients.Group(groupName).ReceiveMessage(GetMessageToSend(groupName)); 
+        }
+
+        public async Task AddUserToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task RemoveUserFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        private string GetMessageToSend(string originalMessage)
+        {
+            return $"User Connection Id : {Context.ConnectionId}. Message : {originalMessage}";
+        }
+
         public override async Task OnConnectedAsync()
         {
              await base.OnConnectedAsync();
